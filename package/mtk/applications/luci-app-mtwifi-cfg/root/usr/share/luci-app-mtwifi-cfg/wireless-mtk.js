c1a57f481951e04c310077e33f4ffca14d6930a8
kalicyh <34980061+kalicyh@users.noreply.github.com>
2026-03-11 15:33:55 +0800

    wifi: expose WPA3-SAE in mtwifi UI

--- package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
+++ package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
@@ -1614,11 +1614,10 @@ return view.extend({
 					crypto_modes.push(['wep-shared', _('WEP Shared Key'),         10]);
 				}
 				else if (hwtype == 'mtwifi') {
-					if (band == '6g') {
 					crypto_modes.push(['sae', 'WPA3-SAE', 31]);
-					}
 					crypto_modes.push(['owe', 'OWE', 1]);
 					if (band != '6g') {
+						crypto_modes.push(['sae-mixed', 'WPA2-PSK/WPA3-SAE Mixed Mode', 30]);
 						crypto_modes.push(['psk2', 'WPA2-PSK', 35]);
 						crypto_modes.push(['psk', 'WPA-PSK', 12]);
 						if (ifmode == 'ap') {
